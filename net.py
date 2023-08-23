960186f96e26884cc8aef21589f0a6a765088080
abh037 <69530041+abh037@users.noreply.github.com>
2023-08-22 20:12:43 -0400

    Add files via upload

--- net.py
+++ net.py
@@ -1,6 +1,5 @@
 import numpy as np
 from random import choices
-from keras import datasets
 np.seterr(all='ignore')
 
 
@@ -13,24 +12,45 @@ class Net():
         else:
             return sig
 
-    def __init__(self, structure):
-        
-        if len(structure) < 3:
-            raise Exception("Network must have at least 3 layers: [input, hidden, ..., output]")
+    def __init__(self, structure = []):
     
-        self.input_size = structure[0]
-        self.num_classes = structure[-1]
-        self.structure = structure
-        self.W = [np.array([])]
-        self.b = [np.array([])]
-        self.a = [np.array([])]
-        self.z = [np.array([])]
-        
-        for i in range(1, len(structure)):
-            self.W.append(np.random.normal(size = (structure[i], structure[i - 1]), scale = 0.1))
-            self.b.append(np.zeros((1, structure[i])))
-            self.a.append(np.array([]))
-            self.z.append(np.array([]))
+        if len(structure) == 0:
+            self.input_size = 0
+            self.num_classes = 0
+            self.structure = []
+            self.W = [np.array([])]
+            self.b = [np.array([])]
+            self.a = [np.array([])]
+            self.z = [np.array([])]
+            
+        elif len(structure) < 3:
+            raise Exception("Network structure must have at least 3 layers: [input, hidden, ..., output]")
+            
+        else:
+            self.input_size = structure[0]
+            self.num_classes = structure[-1]
+            self.structure = structure
+            self.W = [np.array([])]
+            self.b = [np.array([])]
+            self.a = [np.array([])]
+            self.z = [np.array([])]
+            
+            for i in range(1, len(structure)):
+                self.W.append(np.random.normal(size = (structure[i], structure[i - 1]), scale = 0.1))
+                self.b.append(np.zeros((1, structure[i])))
+                self.a.append(np.array([]))
+                self.z.append(np.array([]))
+    def save(self, name):
+        np.save(name + ".npy", np.array([self.W, self.b]), allow_pickle = True)
+    
+    def load(self, name):
+        weights, biases = np.load(name + ".npy", allow_pickle = True)
+        struc = [weights[1].shape[1]]
+        for W in weights[1:]:
+            struc.append(W.shape[0])
+        self.__init__(structure = struc)
+        self.W = weights
+        self.b = biases
 
     def predict(self, X):
         return np.argmax(self.forward(X))
@@ -44,7 +64,16 @@ class Net():
             self.a[i] = self.sigmoid(self.z[i]).T
         return self.a[-1].T
 
-    def train(self, dataset, labels, epochs = 100, batch_size = 1000, verbose = True):
+    def train(self, dataset, labels, epochs = 50, batch_size = 1000, verbose = True):
+    
+        if batch_size > len(dataset):
+            batch_size = len(dataset)
+            
+        if len(self.structure) == 0:
+            struc = [len(np.array(dataset[0]).flatten()), int(2 * len(np.array(dataset[0]).flatten()) / 3), len(np.unique(labels))]
+            self.__init__(structure = struc)
+            print("No network structure specified, using auto-generated structure " + str(struc))
+
         
         if len(np.array(dataset[0]).flatten()) != self.structure[0]:
             raise Exception("Training dataset elements count (" + str(len(np.array(dataset[0]).flatten())) + ") must be the same size as the input layer (" + str(self.structure[0]) + ")")
@@ -58,7 +87,8 @@ class Net():
         for ep in range(epochs):
             
             running_error = 0
-            learning_rate = 0.001
+            
+            learning_rate = 0.0001
             
             for i, (X, y) in choices(list(enumerate(zip(reshaped_dataset, reshaped_labels))), k = batch_size):
             
@@ -76,17 +106,5 @@ class Net():
             
             print("Epoch: " + str(ep + 1) + "/" + str(epochs) + ", Error: " + str(running_error)[:7], end = '\r')
         print()
-        
+    
 
-(trainx, trainy), (testx, testy) = datasets.mnist.load_data()
-
-my_net = Net([784, 1024, 10])
-my_net.train(trainx, trainy, epochs = 50)
-
-avg = 0
-for x, y in zip(testx, testy):
-    if my_net.predict(x) == y:
-        avg += 1
-avg = 100 * avg/len(testx)
-
-print("mnist accuracy: " + str(avg)[:4] + "%")
